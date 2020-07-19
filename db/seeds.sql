b53cfbcec2d4213dac4c311944001ef4c2158dcf
Vins MacBook <vinavarghese@gmail.com>
2020-07-18 22:16:19 -0700

    started working on css. Finish designing

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@ USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES 
-("Royal Red Robin", false), 
+("Royal Red Robin", true), 
 ("Double Quarter Pounder", false), 
 ("Patty Melt", false), 
 ("The Grilled Cheesus", false)
