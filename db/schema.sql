f4498017ca361f9f6f5d416881804b7efb69a7de
Vinny V <VinAVarghese@gmail.com>
2020-07-19 15:37:58 -0700

    Final updates. Working on Heroku and db set up

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ USE burgers_db;
 
 CREATE TABLE burgers 
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR (200),
+    id INT AUTO_INCREMENT NOT NULL ,
+    burger_name VARCHAR (200) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id) 
 );
